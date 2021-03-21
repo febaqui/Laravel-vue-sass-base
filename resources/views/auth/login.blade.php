d41b7347bde2e3e208fa859bd086f25c22cdea38
Fernando Barraza <maniak@FernandoBQ.local>
2021-03-21 13:19:11 -0600

    Added input containers

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,20 @@
     <section class="login">
         <div class="grid-container">
             <div class="grid-x">
-                <div>hola</div>
+                <auth-component>
+                    <form action="{{route('login')}}" method="POST" class="flex-container flex-dir-column">
+                        <div class="input-container">
+                            <label for="email">Your Email</label>
+                            <input type="text" name="email" id="email" placeholder="Enter your email">
+                            <span class="error-message">This field is required</span>
+                        </div>
+                        <div class="input-container">
+                            <label for="password">Your Password</label>
+                            <input type="text" name="password" id="password" placeholder="Enter your password">
+                            <span class="error-message">This field is required</span>
+                        </div>
+                    </form>
+                </auth-component>
             </div>
         </div>
     </section>
